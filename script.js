92b7136735dadac6261a9f1a28708535641704f6
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2023-09-08 17:47:12 +0900

    Update script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function loaded() {
+  const d = new Date();
+  document.getElementById("age").innerHTML =
+    d.getFullYear() -
+    2006 -
+    (Date.now() >= new Date(`${d.getFullYear()}/05/22 0:00`) ? 0 : 1);
+}
+
+function changegaming(tf){
+  document.getElementsByTagName("html")[0].className = tf ? "gamingmode" : ""
+}
+
+function showForm(){
+  document.getElementById("fb").style.display = "none"
+  document.getElementById("form").innerHTML = '<button id="cb" class="rb" onclick="closeForm()">×</button><br /><p class="rb rbt">お問い合わせ内容に加えて、返信先のユーザーNo.も記入してください。</p><br /><iframe src="https://m.kuku.lu/f.php?5dfabb2603"></iframe>'
+}
+
+function closeForm(){
+  document.getElementById("fb").style.display = ""
+  document.getElementById("form").innerHTML = ""
+}
