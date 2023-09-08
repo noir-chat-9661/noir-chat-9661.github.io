2c68a444f151e1033b28485386716d0e2fcad7eb
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2023-09-08 17:36:52 +0900

    Add files via upload

--- script.js
+++
@@ -1,7 +0,0 @@
-function loaded() {
-  const d = new Date();
-  document.getElementById("age").innerHTML =
-    d.getFullYear() -
-    2006 -
-    (Date.now() >= new Date(`${d.getFullYear()}/05/22 0:00`) ? 0 : 1);
-}
