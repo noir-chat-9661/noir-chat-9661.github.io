786df294660752eaa9e876bbe13911c45f58df5d
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2023-10-19 23:25:38 +0900

    script.js を更新

---
+++ status/script.js
@@ -0,0 +1,38 @@
+function check() {
+  fetch("https://eita.f5.si")
+    .then((n) => {
+      document.getElementById("status1").innerHTML = `${
+        n.status == 200 ? "🟢 使用可能" : "🔴 使用不可"
+      }`;
+    })
+    .catch(
+      () => (document.getElementById("status1").innerHTML = "🔴 使用不可")
+    );
+  fetch("http://sub-eita.f5.si")
+    .then((n) => {
+      document.getElementById("status2").innerHTML = `${
+        n.status == 200 ? "🟢 使用可能" : "🔴 使用不可"
+      }`;
+    })
+    .catch(
+      () => (document.getElementById("status2").innerHTML = "🔴 使用不可")
+    );
+  fetch("http://hcqboard.f5.si")
+    .then((n) => {
+      document.getElementById("status3").innerHTML = `${
+        n.status == 200 ? "🟢 使用可能" : "🔴 使用不可"
+      }`;
+    })
+    .catch(
+      () => (document.getElementById("status3").innerHTML = "🔴 使用不可")
+    );
+  fetch("https://hcqwiki.com")
+    .then((n) => {
+      document.getElementById("status4").innerHTML = `${
+        n.status == 200 ? "🟢 使用可能" : "🔴 使用不可"
+      }`;
+    })
+    .catch(
+      () => (document.getElementById("status4").innerHTML = "🔴 使用不可")
+    );
+}
