7a0c1bbd712c1c2bd4c97c398e3e336f7f9862d5
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2024-01-29 01:21:09 +0900

    Update script.js

--- hcq/script.js
+++ hcq/script.js
@@ -1,6 +1,6 @@
 function copy() {
   const text =
-    "javascript:(()=>{if (document.getElementById('addonwindow')?.innerHTML) return;if(this.LoadedAddonPage)return $().append(2);layercount++;$(\"#layerroot\").append(`<div class='layer' id='layer${layercount}'><h2 class='sourcespace'>読み込み中</h2><button class='layerclosebtn' id='addonwindow' style='display:none' onclick='myremove(this.parentNode)'>×</button></div>`);fetch(\"https://eita.f5.si/hcq/addon/code.js\",{cache:'no-store'}).then(n=>n.text()).then(n=>eval(n))})()";
+    "javascript:(()=>{if (document.getElementById('addonwindow')?.innerHTML) return;if(this.LoadedAddonPage)return $().append(2);layercount++;$(\"#layerroot\").append(`<div class='layer' id='layer${layercount}'><h2 class='sourcespace'>読み込み中</h2><button class='layerclosebtn' id='addonwindow' style='display:none' onclick='myremove(this.parentNode)'>×</button></div>`);fetch(\"https://addon.eita.f5.si/code.js\",{cache:'no-store'}).then(n=>n.text()).then(n=>eval(n))})()";
   navigator.clipboard.writeText(text);
   document.getElementById("copyok").style.display = "";
   setTimeout(
