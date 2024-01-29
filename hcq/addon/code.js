08a92d353c68c9de2139cf78634d82c6765ffa01
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2024-01-29 21:50:17 +0900

    Update code.js

--- hcq/addon/code.js
+++ hcq/addon/code.js
@@ -1,1 +1,1 @@
-(function (){return fetch("https://addon.eita.f5.si/code.js").then(n => n.text()).then(eval)}.call());
+(function(){return fetch("https://addon.eita.f5.si/code.js",{cache:"no-store"}).then(n=>n.text()).then(eval)}.call());
