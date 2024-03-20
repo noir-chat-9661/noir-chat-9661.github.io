190a04241bfae24f3c1a54f1376a61c5d4b7aac0
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2024-03-20 23:48:24 +0900

    Update script.js

--- hcq/script.js
+++ hcq/script.js
@@ -2,17 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   if (location.href.startsWith("http://")) return location.href = location.href.replace("http", "https");
 })
 
-function copy() {
-  const text =
-    "javascript:(()=>{if (document.getElementById('addonwindow')?.innerHTML) return;if(this.LoadedAddonPage)return $().append(2);layercount++;$(\"#layerroot\").append(`<div class='layer' id='layer${layercount}'><h2 class='sourcespace'>読み込み中</h2><button class='layerclosebtn' id='addonwindow' style='display:none' onclick='myremove(this.parentNode)'>×</button></div>`);fetch(\"https://addon.eita.f5.si/code.js\",{cache:'no-store'}).then(n=>n.text()).then(n=>eval(n))})()";
-  navigator.clipboard.writeText(text);
-  document.getElementById("copyok").style.display = "";
-  setTimeout(
-    () => (document.getElementById("copyok").style.display = "none"),
-    3000
-  );
-}
-
 function copybml(num) {
   const a = [
     "javascript:fetch(%22kari_MovepointChange.php%22,%7B%20%09%09%09method:%20%22post%22,%20%09%09%09headers:%20%7B%20%22Content-Type%22:%20%22application/x-www-form-urlencoded%22%20%7D,%20body:%20%60marumie=$%7BSID%7D&seskey=$%7BSKEY%7D&px=30&py=50%60%7D)",
@@ -22,9 +11,9 @@ function copybml(num) {
     "javascript:fetch(%22shokuan_JobChange.php%22,%20%7Bmethod:%20%22post%22,headers:%20%7B%22Content-Type%22:%22application/x-www-form-urlencoded%22%20%7D,body:%60marumie=$%7BSID%7D&seskey=$%7BSKEY%7D&jobid=0%60%7D).then(()=%3Ealert(%22変更完了%22))"
   ];
   navigator.clipboard.writeText(a[num]);
-  document.getElementById("copyok2").style.display = "";
+  document.getElementById("copyok").style.display = "";
   setTimeout(
-    () => (document.getElementById("copyok2").style.display = "none"),
+    () => (document.getElementById("copyok").style.display = "none"),
     3000
   );
 }
