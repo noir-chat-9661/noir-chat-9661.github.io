925bbf93b373481b61dc164f12ebef06c02d0e46
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2024-03-22 15:41:36 +0900

    Delete src directory

--- src/preload_ModeSelect.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron/renderer");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-	start: (obj) => ipcRenderer.send("start", obj),
-	ready: () => ipcRenderer.sendSync("ready"),
-});
